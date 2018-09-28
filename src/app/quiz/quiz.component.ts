cc13d8ee4cd5dfe15ab3d250eac1524c545721ea
FrankBuilds <telstarxiii@gmail.com>
2018-09-28 10:29:44 -0700

    Sep 28 commit

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,36 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { QuizService } from '../services/quiz.service';
-import { HelperService } from '../services/helper.service';
-import { Option, Question, Quiz, QuizConfig } from '../models/index';
+import { QuizService } from "../services/quiz.service";
+import { HelperService } from "../services/helper.service";
+import { Option, Question, Quiz, QuizConfig } from "../models/index";
 
 @Component({
-  selector: 'app-quiz',
-  templateUrl: './quiz.component.html',
-  styleUrls: ['./quiz.component.css'],
+  selector: "app-quiz",
+  templateUrl: "./quiz.component.html",
+  styleUrls: ["./quiz.component.css"],
   providers: [QuizService]
 })
 export class QuizComponent implements OnInit {
   quizes: any[];
   quiz: Quiz = new Quiz(null);
-  mode = 'quiz';
+  mode = "quiz";
   quizName: string;
   totalScore: number;
   lastQuestion: boolean;
+  selected: boolean = false;
  
+
   config: QuizConfig = {
-    'allowBack': true,
-    'allowReview': true,
-    'autoMove': true,  // if true, it will move to next question automatically when answered.
-    'duration': 0,  // indicates the time (in secs) in which quiz needs to be completed. 0 means unlimited.
-    'pageSize': 1,
-    'requiredAll': false,  // indicates if you must answer all the questions before submitting.
-    'richText': false,
-    'shuffleQuestions': false,
-    'shuffleOptions': false,
-    'showClock': false,
-    'showPager': true,
-    'theme': 'none'
+    allowBack: true,
+    allowReview: true,
+    autoMove: false, // if true, it will move to next question automatically when answered.
+    duration: 0, // indicates the time (in secs) in which quiz needs to be completed. 0 means unlimited.
+    pageSize: 1,
+    requiredAll: false, // indicates if you must answer all the questions before submitting.
+    richText: false,
+    shuffleQuestions: false,
+    shuffleOptions: false,
+    showClock: false,
+    showPager: true,
+    theme: "none"
   };
 
   pager = {
@@ -41,10 +43,10 @@ export class QuizComponent implements OnInit {
   timer: any = null;
   startTime: Date;
   endTime: Date;
-  ellapsedTime = '00:00';
-  duration = '';
+  ellapsedTime = "00:00";
+  duration = "";
 
-  constructor(private quizService: QuizService) { }
+  constructor(private quizService: QuizService) {}
 
   ngOnInit() {
     this.quizes = this.quizService.getAll();
@@ -57,16 +59,18 @@ export class QuizComponent implements OnInit {
       this.quiz = new Quiz(res);
       this.pager.count = this.quiz.questions.length;
       this.startTime = new Date();
-      this.timer = setInterval(() => { this.tick(); }, 1000);
+      this.timer = setInterval(() => {
+        this.tick();
+      }, 1000);
       this.duration = this.parseTime(this.config.duration);
     });
-    this.mode = 'quiz';
+    this.mode = "quiz";
   }
 
   tick() {
     const now = new Date();
     const diff = (now.getTime() - this.startTime.getTime()) / 1000;
-    if ((this.config.duration != 0) && (diff >= this.config.duration)) {
+    if (this.config.duration != 0 && diff >= this.config.duration) {
       this.onSubmit();
     }
     this.ellapsedTime = this.parseTime(diff);
@@ -75,84 +79,94 @@ export class QuizComponent implements OnInit {
   parseTime(totalSeconds: number) {
     let mins: string | number = Math.floor(totalSeconds / 60);
     let secs: string | number = Math.round(totalSeconds % 60);
-    mins = (mins < 10 ? '0' : '') + mins;
-    secs = (secs < 10 ? '0' : '') + secs;
+    mins = (mins < 10 ? "0" : "") + mins;
+    secs = (secs < 10 ? "0" : "") + secs;
     return `${mins}:${secs}`;
   }
 
   get filteredQuestions() {
-    return (this.quiz.questions) ?
-      this.quiz.questions.slice(this.pager.index, this.pager.index + this.pager.size) : [];
+    return this.quiz.questions
+      ? this.quiz.questions.slice(
+          this.pager.index,
+          this.pager.index + this.pager.size
+        )
+      : [];
   }
 
   onSelect(question: Question, option: Option) {
     if (question.questionTypeId === 1) {
-      question.options.forEach(
-        
-        (x) => { 
-        
+      question.options.forEach(x => {
         if (x.id !== option.id) x.selected = false;
-        if (x.id == option.id) question.score = x.optionScore; 
-        
-        }
-      
-      );
-      
+        if (x.id == option.id) question.score = x.optionScore;
+      });
     }
 
     if (this.config.autoMove) {
-      this.pager.index ++;
+      this.pager.index++;
       this.goTo(this.pager.index);
     }
+
+    this.toggleSelected();
   }
 
   goTo(index: number) {
     if (index >= 0 && index < this.pager.count) {
       this.pager.index = index;
-      this.mode = 'quiz';
+      this.mode = "quiz";
     }
-    
-    if(index == (this.pager.count)){
+
+    if (index == this.pager.count) {
       this.lastQuestion = true;
-      this.mode = 'lastPage';
+      this.mode = "lastPage";
     }
+
+    this.toggleSelected();
   }
 
   isAnswered(question: Question) {
-    return question.options.find(x => x.selected) ? 'Answered' : 'Not Answered';
-  };
+    return question.options.find(x => x.selected) ? "Answered" : "Not Answered";
+  }
+
+  toggleSelected() {
+    // console.log("toggle selected called");
+    this.selected = !(this.selected);
+  }
 
   isCorrect(question: Question) {
-    return question.options.every(x => x.selected === x.isAnswer) ? 'correct' : 'wrong';
-  };
+    return question.options.every(x => x.selected === x.isAnswer)
+      ? "correct"
+      : "wrong";
+  }
 
   onSubmit() {
     let answers = [];
-    this.quiz.questions.forEach(x => answers.push({ 'quizId': this.quiz.id, 'questionId': x.id, 'answered': x.answered }));
+    this.quiz.questions.forEach(x =>
+      answers.push({
+        quizId: this.quiz.id,
+        questionId: x.id,
+        answered: x.answered
+      })
+    );
 
     this.totalScore = this.calculateScore();
 
-    this.mode = 'result';
+    this.mode = "result";
 
     // Post your data to the server here. answers contains the questionId and the users' answer.
     // console.log(this.quiz.questions);
-    
-    
   }
 
   calculateScore() {
     let finalScores: number[] = [];
     let sumup: number = 0;
     this.quiz.questions.forEach(x => finalScores.push(x.score));
-    
+
     console.log(finalScores);
 
     for (var i = 0; i < finalScores.length; i++) {
       sumup += finalScores[i];
     }
-    
+
     return sumup;
   }
-
-  
 }
