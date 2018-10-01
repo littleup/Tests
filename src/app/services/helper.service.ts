7c286e2202a1a007c20a37ee15ffaa93833749c0
FrankBuilds <telstarxiii@gmail.com>
2018-10-01 15:30:18 -0700

    Oct 1 commit

--- src/app/services/helper.service.ts
+++ src/app/services/helper.service.ts
@@ -1,15 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable()
 export class HelperService {
   static toBool(val) {
-    if (val == 'undefined' || val == null || val == '' || val == 'false' || val == 'False')
+    if (
+      val == "undefined" ||
+      val == null ||
+      val == "" ||
+      val == "false" ||
+      val == "False"
+    )
       return false;
-    else // if (val == true || val == 'true' || val == 'True')
-      return true;
-  };
+    // if (val == true || val == 'true' || val == 'True')
+    else return true;
+  }
   static shuffle(array) {
-    var currentIndex = array.length, temp, randomIndex;
+    var currentIndex = array.length,
+      temp,
+      randomIndex;
 
     while (0 !== currentIndex) {
       randomIndex = Math.floor(Math.random() * currentIndex);
@@ -25,14 +33,12 @@ export class HelperService {
     out = out || {};
 
     for (var i = 1; i < arguments.length; i++) {
-      if (!arguments[i])
-        continue;
+      if (!arguments[i]) continue;
 
       for (var key in arguments[i]) {
-        if (arguments[i].hasOwnProperty(key))
-          out[key] = arguments[i][key];
+        if (arguments[i].hasOwnProperty(key)) out[key] = arguments[i][key];
       }
     }
     return out;
-  };
+  }
 }
